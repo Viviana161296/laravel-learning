776b5be9cba22b8544e253e0c1a425920f01870b
Viviana <viviana_flower@hotmail.com>
2019-06-20 22:42:52 -0500

    Show tasks

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,14 @@
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
+|Route::get('/projects','ProjectsController@index');
+Route::post('/projects','ProjectsController@store');
+Route::get('/projects/{project}','ProjectsController@show');
+Route::get('/projects/create','ProjectsController@create');
+Route::get('/projects/{project}/edit','ProjectsController@edit');
+Route::path('/projects/{project}','ProjectsController@update');
+Route::delete('/projects/{project}','ProjectsController@destroy');
+
 */
 
 Route::get('/', function () {
@@ -16,10 +23,5 @@ Route::get('/', function () {
 });
 
 route::resource('projects', 'ProjectsController');
-//Route::get('/projects','ProjectsController@index');
-//Route::post('/projects','ProjectsController@store');
-//Route::get('/projects/{project}','ProjectsController@show');
-//Route::get('/projects/create','ProjectsController@create');
-//Route::get('/projects/{project}/edit','ProjectsController@edit');
-//Route::path('/projects/{project}','ProjectsController@update');
-//Route::delete('/projects/{project}','ProjectsController@destroy');
+
+Route::patch('/tasks/{task}', 'ProjectTaskController@update');
