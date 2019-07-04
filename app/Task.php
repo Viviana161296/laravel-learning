5f84813ba43d9a0ae26569049b4789d19a0a16cb
Viviana Gaona <viviana161296@gmail.com>
2019-07-03 21:31:41 -0500

    Better encapsulation

--- app/Task.php
+++ app/Task.php
@@ -8,6 +8,16 @@ class Task extends Model
 {
     protected $guarded=[];
 
+    public function complete($completed=true)
+    {
+        $this->update(compact('completed'));
+    }
+
+    public function incomplete()
+    {
+        $this->complete(false);
+    }
+
     public function project()
     {
 
